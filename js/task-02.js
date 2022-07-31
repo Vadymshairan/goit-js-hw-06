6a07fc2de655215deba2a2d99ce974bc229ed861
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-07-31 20:23:17 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ulEl = document.querySelector('#ingredients');
+const arrayItems = [];
+const itemEl = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+
+  arrayItems.push(itemEl);
+});
+
+// console.log(itemEl);
+// console.log(arrayItems);
+ulEl.append(...arrayItems);
