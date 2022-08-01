8c460b236b0279454f603eb9d96eba8a570d64e7
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-02 00:58:42 +0300

    task-04

--- js/task-02.js
+++ js/task-02.js
@@ -2,15 +2,30 @@ const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Con
 
 const ulEl = document.querySelector('#ingredients');
 
-const element = ingredients.map(ingredient => {
-  const itemEl = document.createElement('li');
-  itemEl.textContent = ingredient;
-  itemEl.classList.add('item');
-  // console.log(itemEl);
+// const element = ingredients.map(ingredient => {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = ingredient;
+//   itemEl.classList.add('item');
+//   // console.log(itemEl);
 
-  return itemEl;
-});
+//   return itemEl;
+// });
 
-console.log(element);
-// console.log(arrayItems);
-ulEl.append(...element);
+// console.log(element);
+// // console.log(arrayItems);
+// ulEl.append(...element);
+
+const makeItemsList = options => {
+  return options.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+
+    return itemEl;
+  });
+};
+
+const elements = makeItemsList(ingredients);
+console.log(elements);
+
+ulEl.append(...elements);
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,17 @@
-const counterValue = 0;
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+// console.log(decrementBtn);
+const incrementBtn = document.querySelector('[data-action="increment"]');
+// console.log(incrementBtn);
+const counterValueEll = document.querySelector('#value');
+// console.log(counterValueEll.textContent);
+
+decrementBtn.addEventListener('click', () => (counterValueEll.textContent = counterValue -= 1));
+
+// function onDecrementBtnClick(params) {
+//   counterValue -= 1;
+// }
+
+incrementBtn.addEventListener('click', () => (counterValueEll.textContent = counterValue += 1));
+// function onIncrementBtnClick(params) {}
