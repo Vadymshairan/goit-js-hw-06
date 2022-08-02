c9067a95f18b9e2ed6149d34d774ea6a18060db0
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-02 06:33:27 +0300

    task-09 ver0.1_beta

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// console.log(getRandomHexColor());
+const bodyRef = document.querySelector('body');
+// console.log((bodyRef.style.backgroundColor = 'teal'));
+const btnChangeColorRef = document.querySelector('.change-color');
+console.log(btnChangeColorRef);
+const spanColorRef = document.querySelector('.color');
+console.log(spanColorRef.textContent);
+
+btnChangeColorRef.addEventListener('click', onBtnChangeColorClick);
+
+function onBtnChangeColorClick(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanColorRef.textContent = getRandomHexColor();
+}
