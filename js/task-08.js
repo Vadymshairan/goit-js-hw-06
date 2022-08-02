0bbb3b8e919abdcba6ee386ea2f00a9e4939b48c
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-02 05:36:54 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onSubmitBtn);
+
+function onSubmitBtn(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
